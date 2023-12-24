6eb4306b3f4d06efebee75b5c27e2dbfabe2e2b2
JoshPham <JTPham911@gmail.com>
2023-12-24 09:26:57 +0000

    made letter answer nullable

--- app/models.py
+++ app/models.py
@@ -33,12 +33,12 @@ class Lesson(models.Model):
 class Problem(models.Model):
     lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     text_question = models.TextField()
-    num_answer = models.IntegerField()
+    num_answer = models.IntegerField(null=True)
     answer_a = models.CharField(max_length=255, null=True)
     answer_b = models.CharField(max_length=255, null=True)
     answer_c = models.CharField(max_length=255, null=True)
     answer_d = models.CharField(max_length=255, null=True)
-    letter_answer = models.CharField(max_length=1, default='a')
+    letter_answer = models.CharField(max_length=1, default='a', null=True)
 
     def __str__(self):
         return f"Question: {self.text_question} (Lesson: {self.lesson.title}, Unit: {self.lesson.unit.title}, Grade: {self.lesson.unit.grade.title})"
